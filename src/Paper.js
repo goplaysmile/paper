b7ce32c254386c6f8f01557c962732f6f5f3895b
Alexander Matulionis <awmatulionis@Alexanders-MacBook-Pro.local>
2019-05-14 06:52:36 -0700

    Loading in firestore values for table.

--- src/Paper.js
+++ src/Paper.js
@@ -1,15 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import firebaseConfig from './.firebase.config.json'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+function useUser() {
+  const [ user, setUser ] = useState(firebase.auth().currentUser)
+  
+  if (!user) {
+    firebase.auth().signInAnonymously().then(setUser)
+  }
+
+  return user
+}
+
+function useTable(user) {
+  const [ table, setTable ] = useState()
+
+  if (!user || !user.uid) return
+  
+  firebase.firestore().collection('paper').doc(user.uid).get()
+    .then(d => setTable(d.data()))
+
+  return table
+}
 
 function Paper({ w, h }) {
+  const user = useUser()
+  const table = useTable(user)
+
+  console.log(JSON.stringify(table, null, 2))
+
   return (
     <table>
       <tbody>
         {
-          [...Array(h).keys()].map(y =>
+          (table || [...Array(h).keys()].map(() => undefined) ).map((r, y) =>
             <tr key={y}>
               {
-                [...Array(w).keys()].map(x =>
-                  <td key={x} contentEditable></td>
+                (r || [...Array(w).keys()].map(() => undefined)).map((c, x) =>
+                  <td key={x} contentEditable>{c}</td>
                 )
               }
             </tr>
