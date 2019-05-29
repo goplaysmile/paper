2a1a2f65fcf6367e30807e048a7e3b8dac97fe89
Alexander Matulionis <awmatulionis@Alexanders-MacBook-Pro.local>
2019-05-29 06:54:24 -0700

    DI for Paper.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import useUser from './User'
+import useTable from './Table'
 import Paper from './Paper'
 import styles from './App.module.scss'
 
 function App() {
+  const user = useUser()
+  const table = useTable(user, 6, 9)
+  
   return (
     <div className={styles.App}>
       <div>🗒</div>
-      <Paper w={6} h={9} />
+      <Paper
+        grid={table}
+        onEdit={(row, col, txt) => {
+          console.log(`before ${JSON.stringify(table)}`)
+          table[row][col] = txt
+          console.log(`after ${JSON.stringify(table)}`)
+          
+          firebase.firestore()
+            .collection('paper')
+            .doc(user.uid)
+            .set(Object.assign({}, table))
+        }}
+      />
     </div>
   )
 }
--- src/Paper.js
+++ src/Paper.js
@@ -1,48 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import ContentEditable from 'react-contenteditable'
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
-import firebaseConfig from './.firebase.config.json'
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig)
-}
-
-function useUser() {
-  const [ user, setUser ] = useState(firebase.auth().currentUser)
-  
-  useEffect(() => {
-    if (user) return
-
-    firebase.auth().signInAnonymously().then(({ user }) => {
-      setUser(user)
-    })
-  }, [user])
-
-  return user
-}
-
-function useTable(user, w, h) {
-  const [ table, setTable ] = useState()
-
-  useEffect(() => {
-    if (!user || !user.uid) return
-
-    firebase.firestore().collection('paper').doc(user.uid).onSnapshot(snap => {
-      console.log(`arrived\n${JSON.stringify(snap.data())}`)
-      setTable(snap.data() ? Object.values(snap.data()) : undefined)
-    })
-  }, [user])
-    
-  return table || [...Array(h).keys()].map(() => [...Array(w).keys()].map(() => ''))
-}
-
-function Paper({ w, h, disabled }) {
-  console.warn(`Paper render! w=${w} h=${h} disabled=${!!disabled}`)
-
-  const user = useUser()
-  const table = useTable(user, w, h)
+function Paper({ grid, onEdit, disabled }) {
   const [ buf, setBuf ] = useState()
 
   console.log(`buf ${JSON.stringify(buf)}`)
@@ -51,29 +10,27 @@ function Paper({ w, h, disabled }) {
     <table>
       <tbody>
         {
-          table.map((r, y) =>
-            <tr key={y}>
+          grid.map((r, row) =>
+            <tr key={row}>
               {
-                r.map((c, x) =>
+                r.map((c, col) =>
                   <td
-                    key={x}
+                    key={col}
                     
                     onBlur={() => {
-                      if (disabled || !buf) return
-
-                      table[y][x] = buf[y][x]
-                      firebase.firestore().collection('paper').doc(user.uid).set(Object.assign({}, table))
+                      if (disabled || !buf || !buf[row] || !buf[row][col]) return
+                      onEdit(row, col, buf[row][col])
                       setBuf()
                     }}
                   >
                     {
                       disabled ? c : (
                         <ContentEditable
-                          html={((buf || {})[y] || {})[x] || c}
+                          html={((buf || {})[row] || {})[col] || c}
 
                           onChange={e => {
-                            let b = buf || {[y]: {}}
-                            b[y][x] = e.target.value
+                            let b = buf || {[row]: {}}
+                            b[row][col] = e.target.value
                             setBuf(b)
                           }}
                         />
